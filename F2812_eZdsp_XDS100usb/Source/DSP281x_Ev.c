3d01ec9463c4e78c46929b98a8b08072f55a5f30
javi <javimusama@gmail.com>
2023-07-04 18:51:21 +0200

    PWM1 and 2 less code
    pwm 1 and 2 funcionan, pero no entiendo muy bien por qué o como funciona tim2, el deadtime todavia no hace efecto

--- F2812_eZdsp_XDS100usb/Source/DSP281x_Ev.c
+++ F2812_eZdsp_XDS100usb/Source/DSP281x_Ev.c
@@ -14,7 +14,7 @@
 #include "DSP281x_Device.h"     // DSP281x Headerfile Include File
 #include "DSP281x_Examples.h"   // DSP281x Examples Include File
 
-#define pwmperiodinticks (Uint16)2500/2 //POR QUE ME SALE QUE EL CLOCK DEL SISTEMA ES 50Mhz en vez de 25Mhz
+#define pwmperiodinticks (Uint16)2500/2 //como es nidireccional count up down es /2
 //---------------------------------------------------------------------------
 // InitEv: 
 //---------------------------------------------------------------------------
@@ -22,37 +22,29 @@
 //
 void InitEv(void)
 {
-    //PWM1 setup
-    // Peripheral clock enables set for the selected peripherals.
-       EALLOW;
-       SysCtrlRegs.PCLKCR.bit.EVAENCLK=1;//1 enable - 0 disable
-       EDIS;
+     //PWM1 setup
+     // Peripheral clock enables set for the selected peripherals.
+     EALLOW;
+     SysCtrlRegs.PCLKCR.bit.EVAENCLK=1;//1 enable - 0 disable
+     EDIS;
+
+     // Initialization of the dead band timer control register //TODO el deadtime marca 1,5us siempre , no cambia con DBT, el DBT no funciona??
+     EvaRegs.DBTCONA.bit.EDBT1 = 1; // Enable dead band timer 1
+     EvaRegs.DBTCONA.bit.DBT   = 50; // Set the DB Timer period to 50
+     EvaRegs.DBTCONA.bit.DBTPS = 0; // Set the dead band prescaler to 1
+     //deadtime to 1*50*0,04us=2us
+
     //compare action control register
      EvaRegs.ACTRA.bit.CMP1ACT = 0x002; // Pin Action on PWM1 active high
      EvaRegs.ACTRA.bit.CMP2ACT = 0x001; // Pin Action on PWM2 active low
-     EvaRegs.ACTRA.bit.D       = 0x000; // SV not in use
-     EvaRegs.ACTRA.bit.SVRDIR  = 0x000; // SV not in use
-
-   //  EvaRegs.ACTRA.all = 0x0096; // Silicon Errata //what
-
-    // Initialization of the dead band timer control register
-    EvaRegs.DBTCONA.bit.EDBT1 = 1; // Enable dead band timer 1
-    EvaRegs.DBTCONA.bit.EDBT2 = 1; // Enable dead band timer 2
-    EvaRegs.DBTCONA.bit.DBT   = 50; // Set the DB Timer period to 50
-    EvaRegs.DBTCONA.bit.DBTPS = 0; // Set the dead band prescaler to 1
-    //deadtime to 1*50*0,04us=2us
 
     // Initialization of timers and compare points
     EvaRegs.T1PR                = pwmperiodinticks; //25Mhz/2500=10Khz
-    EvaRegs.T2PR                = pwmperiodinticks; //25Mhz/2500=10Khz
+    EvaRegs.CMPR1               = pwmperiodinticks/2; //50%
+
     EvaRegs.EXTCONA.bit.INDCOE  = 1; // Independent Compare Output Enabled
-    EvaRegs.T1CMPR              = pwmperiodinticks/2;
-    EvaRegs.CMPR1               = pwmperiodinticks/2;
-    EvaRegs.T2CMPR              = pwmperiodinticks/2;
-    EvaRegs.CMPR2               = pwmperiodinticks/2;
 
     EvaRegs.COMCONA.bit.C1TRIPE         = 1; // Full Compare 1 Trip enabled
-    EvaRegs.COMCONA.bit.C2TRIPE         = 1; // Full Compare 2 Trip enabled
     EvaRegs.COMCONA.bit.FCMP1OE         = 1; // Full Compare 1 Output enabled
     EvaRegs.COMCONA.bit.PDPINTASTATUS   = 0; // PDPint High
     EvaRegs.COMCONA.bit.SVENABLE        = 0; // SVPWM disabled
@@ -68,21 +60,6 @@ void InitEv(void)
     EvaRegs.T1CON.bit.TMODE     = 1; // Continuous up-down counting
     EvaRegs.T1CON.bit.FREE      = 1; // Free run mode
     EvaRegs.T1CON.bit.SOFT      = 0;
-
-    // Initialization of the Timer 1 control registers
-    EvaRegs.T2CON.bit.TECMPR    = 1; // Timer 1 Compare enabled
-    EvaRegs.T2CON.bit.TCLD10    = 1; // Compare to reload when counter equals zero or period register
-    EvaRegs.T2CON.bit.TCLKS10   = 0; // Timer 1 Clock source set to internal (HSPCLK)
-    EvaRegs.T2CON.bit.TENABLE   = 1; // Timer 1 enabled
-    EvaRegs.T2CON.bit.TPS       = 0; // preescaler to 1, 25Mhz
-    EvaRegs.T2CON.bit.TMODE     = 1; // Continuous up-down counting
-    EvaRegs.T2CON.bit.FREE      = 1; // Free run mode
-    EvaRegs.T2CON.bit.SOFT      = 0;
-
-
-
-
-
 
 }	
 	
