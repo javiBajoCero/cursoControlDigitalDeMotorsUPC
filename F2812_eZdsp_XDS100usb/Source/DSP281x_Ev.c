60cc4fb8737d71238d5151da7f368ffc8d8a00ce
javi <javimusama@gmail.com>
2023-07-04 17:07:59 +0200

    PWM1 y 2
    PWM 1 and PWM2 funcionan , con deadtime

--- F2812_eZdsp_XDS100usb/Source/DSP281x_Ev.c
+++ F2812_eZdsp_XDS100usb/Source/DSP281x_Ev.c
@@ -14,7 +14,7 @@
 #include "DSP281x_Device.h"     // DSP281x Headerfile Include File
 #include "DSP281x_Examples.h"   // DSP281x Examples Include File
 
-#define pwmperiodinticks (Uint16)2500/2
+#define pwmperiodinticks (Uint16)2500/2 //POR QUE ME SALE QUE EL CLOCK DEL SISTEMA ES 50Mhz en vez de 25Mhz
 //---------------------------------------------------------------------------
 // InitEv: 
 //---------------------------------------------------------------------------
@@ -29,6 +29,7 @@ void InitEv(void)
        EDIS;
     //compare action control register
      EvaRegs.ACTRA.bit.CMP1ACT = 0x002; // Pin Action on PWM1 active high
+     EvaRegs.ACTRA.bit.CMP2ACT = 0x001; // Pin Action on PWM2 active low
      EvaRegs.ACTRA.bit.D       = 0x000; // SV not in use
      EvaRegs.ACTRA.bit.SVRDIR  = 0x000; // SV not in use
 
@@ -36,15 +37,22 @@ void InitEv(void)
 
     // Initialization of the dead band timer control register
     EvaRegs.DBTCONA.bit.EDBT1 = 1; // Enable dead band timer 1
+    EvaRegs.DBTCONA.bit.EDBT2 = 1; // Enable dead band timer 2
     EvaRegs.DBTCONA.bit.DBT   = 50; // Set the DB Timer period to 50
     EvaRegs.DBTCONA.bit.DBTPS = 0; // Set the dead band prescaler to 1
     //deadtime to 1*50*0,04us=2us
 
     // Initialization of timers and compare points
     EvaRegs.T1PR                = pwmperiodinticks; //25Mhz/2500=10Khz
+    EvaRegs.T2PR                = pwmperiodinticks; //25Mhz/2500=10Khz
     EvaRegs.EXTCONA.bit.INDCOE  = 1; // Independent Compare Output Enabled
+    EvaRegs.T1CMPR              = pwmperiodinticks/2;
+    EvaRegs.CMPR1               = pwmperiodinticks/2;
+    EvaRegs.T2CMPR              = pwmperiodinticks/2;
+    EvaRegs.CMPR2               = pwmperiodinticks/2;
 
     EvaRegs.COMCONA.bit.C1TRIPE         = 1; // Full Compare 1 Trip enabled
+    EvaRegs.COMCONA.bit.C2TRIPE         = 1; // Full Compare 2 Trip enabled
     EvaRegs.COMCONA.bit.FCMP1OE         = 1; // Full Compare 1 Output enabled
     EvaRegs.COMCONA.bit.PDPINTASTATUS   = 0; // PDPint High
     EvaRegs.COMCONA.bit.SVENABLE        = 0; // SVPWM disabled
@@ -61,9 +69,19 @@ void InitEv(void)
     EvaRegs.T1CON.bit.FREE      = 1; // Free run mode
     EvaRegs.T1CON.bit.SOFT      = 0;
 
+    // Initialization of the Timer 1 control registers
+    EvaRegs.T2CON.bit.TECMPR    = 1; // Timer 1 Compare enabled
+    EvaRegs.T2CON.bit.TCLD10    = 1; // Compare to reload when counter equals zero or period register
+    EvaRegs.T2CON.bit.TCLKS10   = 0; // Timer 1 Clock source set to internal (HSPCLK)
+    EvaRegs.T2CON.bit.TENABLE   = 1; // Timer 1 enabled
+    EvaRegs.T2CON.bit.TPS       = 0; // preescaler to 1, 25Mhz
+    EvaRegs.T2CON.bit.TMODE     = 1; // Continuous up-down counting
+    EvaRegs.T2CON.bit.FREE      = 1; // Free run mode
+    EvaRegs.T2CON.bit.SOFT      = 0;
 
-    EvaRegs.T1CMPR  = pwmperiodinticks/2;
-    EvaRegs.CMPR1   = pwmperiodinticks/2;
+
+
+
 
 
 }	
